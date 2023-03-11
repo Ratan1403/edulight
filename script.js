3a36ae53ca10c1b5e71b73788ccb60d5695b21cd
Ratan1403 <127569272+Ratan1403@users.noreply.github.com>
2023-03-11 06:12:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let display = document.querySelector('.display');
+
+function addToDisplay(val) {
+  display.value += val;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
+
